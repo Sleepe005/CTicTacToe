f6fe32ed60c38c9a8e429f7d987429c98f7aabcf
Sleepe005 <kholopov005@mail.ru>
2024-10-26 23:07:13 +0300

    feat: add functions for get, split and validation input data

--- main.c
+++ main.c
@@ -6,6 +6,99 @@
 #include <unistd.h>
 #include <string.h>
 
+void Split(char data[6], int *value){
+    char element[3] = "";
+    int pos = 0;
+    int j = 0;
+    for(int i = 0; i < strlen(data); ++i){
+        if(!isdigit(data[i])){
+            value[j] = atoi(element);
+            j++;
+            strcpy(element, "");
+            pos = 0;
+        }else{
+            element[pos] = data[i];
+            pos++;
+        }
+    }
+    value[j] = atoi(element);
+}
+
+bool isSample(char strData[6]){
+    bool allDigIsDig = true;
+    bool hasSep = true; // __.__.____
+    for(int i = 0; i < strlen(strData); ++i){
+        // TODO: Разрешить на вход цифры
+        if(i == 2){
+            if(!ispunct(strData[i])){
+                hasSep = false;
+            }
+        }else if(!isdigit(strData[i]))
+            allDigIsDig = false;
+        }
+    return allDigIsDig && hasSep;
+}
+
+bool getValuesForField(int* sizeX, int* sizeY, int* i_coutForWin){
+    // Получаем размер игрового поля
+    clear();
+    printw("Введите размеры игрового поля в формате <X>:<Y>\n(Два числа разделённых двоеточием):");
+
+    char sizes[6];
+    scanw("%s", &sizes);
+
+    // Обрабатываем неверный формат ввода данных
+    // if(!isSample(sizes)){
+    //     clear();
+    //     printw("Неверный формат входных данных");
+    //     getch();
+    //     return false;
+    // }
+
+    int sizesValues[2];
+    Split(sizes, sizesValues);
+    *sizeX = sizesValues[0];
+    *sizeY = sizesValues[1];
+
+    // Обрабатываем нееверные числовые значения данных
+    if(*sizeX < 4 || *sizeY < 4 || *sizeX > 20 || *sizeY > 20){
+        clear();
+        printw("Минимальный размер - 4, максимальный размер - 20");
+        getch();
+        *sizeX = 0;
+        *sizeY = 0;
+        return false;
+    }
+
+    // Получаем количество элементов, необходимых для победы
+    clear();
+    printw("Введи количество крестиков или ноликов, необходимых для победы");
+
+    char coutForWin[3];
+    scanw("%s", &coutForWin);
+
+    // Обрабатываем неверный формат ввода данных
+    bool goodInputCout = true;
+    for(int i = 0; i < strlen(coutForWin); i++){
+        if(!isdigit(coutForWin[i])){
+            goodInputCout = false;
+        }
+    }
+
+    if(!goodInputCout || strlen(coutForWin) == 0){
+        clear();
+        printw("Неверный формат данных");
+        getch();
+        strcpy(coutForWin, "");
+        return false;
+    }
+
+    *i_coutForWin = atoi(coutForWin);
+
+    return true;
+}
+
+
 int main(){
     setlocale(LC_ALL, "");
 
