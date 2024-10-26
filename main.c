557c8d7e96ec8068cbedc6babb56fbdd86f2a0f9
Sleepe005 <kholopov005@mail.ru>
2024-10-26 23:05:20 +0300

    feat: init project, add libraties and language settings

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <ncurses.h>
+#include <locale.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
 int main(){
+    setlocale(LC_ALL, "");
+
     return 0;
 }
